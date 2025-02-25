6483c3992f2581ed47d677f9c6f536e807ac42e1
Bhavatharani Govind <bhavatharani_govind@epam.com>
2025-02-25 14:52:50 +0530

    Added string builder

---
+++ StringBuilder/HotelBookingSystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace HotelBookingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder message = new StringBuilder();
+            message.AppendLine("Welcome to the ITC Grand Hotel");
+            message.AppendLine("We hope you enjoy your stay");
+
+            string upperMessage = message.ToString().ToUpper();
+            string replacedMessage = upperMessage.Replace("ITC GRAND", "LUXURY INN");
+
+            Console.WriteLine(replacedMessage);
+            Console.ReadLine();
+        }
+    }
+}
